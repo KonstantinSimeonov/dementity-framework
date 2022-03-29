80c7c807883aefe6b8f74d880a4056c3fc69bbad
Konstantin Simeonov <kon.simeonov@protonmail.com>
2022-03-29 12:20:46 +0300

    add moar stuff

--- src/columns/definitions.ts
+++ src/columns/definitions.ts
@@ -1,17 +1,27 @@
-export const varchar = (length: number) =>
+// TODO: union type
+type CommonColumnConstraints = {
+  not_null?: boolean
+  primary_key?: true
+  unique?: boolean
+}
+
+export const varchar = (length: number, constraints: CommonColumnConstraints = {}) =>
   ({
+    constraints,
     length,
     type: `varchar`,
     sql_type: `varchar(${length})`,
   } as const);
 
-export const integer = () =>
+export const integer = (constraints: CommonColumnConstraints = {}) =>
   ({
+    constraints,
     type: `integer`,
     sql_type: `integer`,
   } as const);
 
-export const boolean = () => ({
+export const boolean = (constraints: CommonColumnConstraints = {}) => ({
+  constraints,
   type: `boolean`,
   sql_type: `boolean`
 }) as const
--- src/demo.ts
+++ src/demo.ts
@@ -1,19 +1,27 @@
-import { varchar, integer, boolean } from "./columns";
+import { varchar, integer, boolean, ColumnType } from "./columns";
 import { create_model } from "./columns/models";
 import { AnyTable } from "./types";
 import { make_schema } from "./table"
 import { entries } from "./util/entries";
 
 const sc = make_schema(`users`, {
-  id: varchar(30),
-  name: varchar(30),
+  id: varchar(30, { primary_key: true }),
+  name: varchar(30, { not_null: true }),
   age: integer(),
   admin: boolean()
 });
 
+const handle_constraints = ({ constraints, sql_type }: ColumnType) =>
+  [
+    sql_type,
+    constraints.not_null && `not null`,
+    constraints.unique && `unique`,
+    constraints.primary_key && `primary key`
+  ].filter(Boolean).join(` `)
+
 const create_table = <Table extends AnyTable>(table: Table) =>
   `create table ${table.name} (\n${entries(table.schema)
-    .map(([name, f]) => `  ${name} ${f.sql_type}`)
+    .map(([name, f]) => `  ${name} ${handle_constraints(f)}`)
     .join(`,\n`)}\n);`;
 
 const m = create_model(sc);
